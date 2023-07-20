91a652703ecbd55eaa82c0631b7287f54613699d
Ajulll22 <ajulrizki@gmail.com>
2023-07-20 21:27:24 +0700

    last

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EventModel;
+use App\Models\ParticipantModel;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -12,9 +14,18 @@ class DashboardController extends Controller
     }
     
     function index() {
-        $data["menu"] = $this->menu;
-        $data["sub_menu"] = $this->sub_menu;
-        
+        $balita = ParticipantModel::where("type", 1)->count();
+        $remaja = ParticipantModel::where("type", 2)->count();
+        $lansia = ParticipantModel::where("type", 3)->count();
+
+        $data = [
+            "menu" => $this->menu,
+            "sub_menu" => $this->sub_menu,
+            "balita" => $balita,
+            "remaja" => $remaja,
+            "lansia" => $lansia,
+        ];
+
         return view('index', $data);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,12 +4,71 @@
 
 @section('page-script')
     <script>
-      $("#test").click( function () {  
-        console.log("test");
-      } )
     </script>
 @endsection
 
 @section('content')
-    <h3 id="test">test</h3>
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+      <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+  </div>
+
+  <!-- Content Row -->
+  <div class="row">
+
+      <!-- Earnings (Monthly) Card Example -->
+      <div class="col mb-4">
+          <div class="card border-left-primary shadow h-100 py-2">
+              <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                          <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                              Peserta Balita</div>
+                          <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $balita }}</div>
+                      </div>
+                      <div class="col-auto">
+                          <i class="fas fa-baby fa-2x text-gray-300"></i>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      <!-- Earnings (Monthly) Card Example -->
+      <div class="col mb-4">
+          <div class="card border-left-success shadow h-100 py-2">
+              <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                          <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                              Peserta Remaja</div>
+                          <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $remaja }}</div>
+                      </div>
+                      <div class="col-auto">
+                          <i class="fas fa-child fa-2x text-gray-300"></i>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      <!-- Earnings (Monthly) Card Example -->
+      <div class="col mb-4">
+          <div class="card border-left-info shadow h-100 py-2">
+              <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                Peserta Lansia</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $lansia }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-person-booth fa-2x text-gray-300"></i>
+                        </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      <!-- Pending Requests Card Example -->
+  </div>
 @endsection
